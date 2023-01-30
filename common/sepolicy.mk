ArrowOS/android_device_arrow_sepolicy

sepolicy: Drop Lineage Trust policy
Change-Id: Ia9fa7aa8638859de241219b440dc6f2191192ee1

common/sepolicy.mk
@@ -23,8 +23,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/system
+    device/arrow/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/arrow/sepolicy/common/dynamic \

CrystalOS-Temp/device_crystal

common/sepolicy.mk
@@ -23,8 +23,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/crystal/sepolicy/common/dynamic \
-    device/crystal/sepolicy/common/system
+    device/crystal/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/crystal/sepolicy/common/dynamic \
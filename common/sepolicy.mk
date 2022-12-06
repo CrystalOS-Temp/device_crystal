bc70eb904289e0f4c23ac684c38743b006491fa8
Sonal Singh <sonal.singh.19993@gmail.com>
2022-12-06 14:32:16 +0530

    sepolicy: Drop Lineage Trust policy
    Change-Id: Ia9fa7aa8638859de241219b440dc6f2191192ee1

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -23,8 +23,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/crystal/sepolicy/common/dynamic \
-    device/crystal/sepolicy/common/system
+    device/crystal/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/crystal/sepolicy/common/dynamic \
